386f35d110cdc7c49bbb82e11556722f322a21f0
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-05-16 17:49:46 +0400

    updated gitignore and switched to warp framework

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use actix_web::{App, HttpServer};
-
-use chat_server_rpc::chat_server_server::{ChatServer, ChatServerServer};
-use chat_server_rpc::{CreateRoomRequest, CreateRoomResponse};
-use tonic::transport::Server;
-use tonic::{Request, Response, Status};
-
-mod server;
+use chat_server_rpc::{
+    chat_server_server::{ChatServer, ChatServerServer},
+    CreateRoomRequest, CreateRoomResponse,
+};
+use tonic::{transport::Server, Request, Response, Status};
+use warp::{ws::WebSocket, Filter};
 
 pub mod chat_server_rpc {
     tonic::include_proto!("chatserver");
@@ -16,7 +14,10 @@ pub struct ChatServerService {}
 
 #[tonic::async_trait]
 impl ChatServer for ChatServerService {
-    async fn create_room(&self, request: Request<CreateRoomRequest>) -> Result<Response<CreateRoomResponse>, Status> {
+    async fn create_room(
+        &self,
+        request: Request<CreateRoomRequest>,
+    ) -> Result<Response<CreateRoomResponse>, Status> {
         let req = request.into_inner();
         println!("Creating new room for event {}", req.event_code);
 
@@ -27,16 +28,23 @@ impl ChatServer for ChatServerService {
     }
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let chat_server_service = ChatServerService::default();
     Server::builder()
         .add_service(ChatServerServer::new(chat_server_service))
-        .serve("[::1]:50051".parse().unwrap())
-        .await.unwrap();
+        .serve("[::1]:50051".parse()?)
+        .await?;
 
-    HttpServer::new(|| App::new())
-        .bind(("0.0.0.0", 8060))?
-        .run()
-        .await
+    let chat = warp::path("chat")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| ws.on_upgrade(|sock| user_connected(sock)));
+
+    warp::serve(chat).run(([0, 0, 0, 0], 8060)).await;
+    Ok(())
+}
+
+async fn user_connected(ws: WebSocket) {
+    // TODO
+    println!("New WS connection");
 }
